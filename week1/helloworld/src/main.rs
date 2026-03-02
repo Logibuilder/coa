446b90c954ea6b620a95b3feab5ba29ff1a0d165
Assane KANE <assane.kane2.etu.univ-lille.fr>
2026-03-02 16:21:14 +0100

    2.1.4. Password check ok)

--- week1/helloworld/src/main.rs
+++ week1/helloworld/src/main.rs
@@ -10,4 +10,20 @@ fn main() {
         println!("y = {}", y);
         y += 1;
     }
+
+    let mut passworld = String::new();
+    println!("Enter your password: ");
+    std::io::stdin().read_line(&mut passworld).expect("Error reading password");
+    check_password(&passworld.trim());
+}
+
+
+pub fn check_password(passworld: &str) { 
+    let correct_password = "secret";
+    
+    if passworld == correct_password {
+        println!("Good");
+    } else {
+        println!("Wrong");
+    }
 }
